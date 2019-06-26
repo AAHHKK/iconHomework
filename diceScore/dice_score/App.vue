ce147d024052bd3895557858fd988f241d21df49
AN HONG KYUNG <ahk959595@gmail.com>
2019-06-26 23:52:20 +0900

    5,6주차 다이스ui추가

---
+++ diceScore/dice_score/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+      <slot>
+      </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {};
+  },
+  components: {
+    
+  },
+  methods: {}
+};
+</script>
+
+<style>
+body,
+h1 {
+  font-family: "Raleway", sans-serif;
+}
+body,
+html {
+  height: 100%;
+}
+</style>
